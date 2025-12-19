9f00601670c0f606e92898a91591341ef6c1bb64
EgeSayinbas <egesayinbas@gmail.com>
2025-12-20 00:58:16 +0300

    add network-synced emotes and gold mining 2d ui asset
    - Added network-synchronized emotes triggered by key inputs
      - Key 4 plays Anim_Slay once
      - Key 5 plays Anim_Thumbsup once
    -gold-mining-ui-2d - ferro-entertainment

--- Assets/Scripts/NetworkPlayerEmotes.cs
+++ Assets/Scripts/NetworkPlayerEmotes.cs
@@ -9,9 +9,13 @@ public class NetworkPlayerEmotes : NetworkBehaviour
     [Header("Trigger Names (Animator)")]
     [SerializeField] private string trigHead360 = "Trig_Head360";
     [SerializeField] private string trigChew = "Trig_Chew";
+    [SerializeField] private string trigSlay = "Trig_Slay";
+    [SerializeField] private string trigThumbsup = "Trig_Thumbsup";
 
     private int _hashHead360;
     private int _hashChew;
+    private int _hashSlay;
+    private int _hashThumbsup;
 
     private void Awake()
     {
@@ -20,6 +24,8 @@ public class NetworkPlayerEmotes : NetworkBehaviour
 
         _hashHead360 = Animator.StringToHash(trigHead360);
         _hashChew = Animator.StringToHash(trigChew);
+        _hashSlay = Animator.StringToHash(trigSlay);
+        _hashThumbsup = Animator.StringToHash(trigThumbsup);
     }
 
     private void Update()
@@ -33,12 +39,21 @@ public class NetworkPlayerEmotes : NetworkBehaviour
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
             RequestEmoteServerRpc(EmoteType.Chew);
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            RequestEmoteServerRpc(EmoteType.Slay);
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+            RequestEmoteServerRpc(EmoteType.Thumbsup);
+
     }
 
     private enum EmoteType : byte
     {
         Head360 = 0,
-        Chew = 1
+        Chew = 1,
+        Slay=2,
+        Thumbsup=3,
     }
 
     [ServerRpc]
@@ -64,6 +79,16 @@ public class NetworkPlayerEmotes : NetworkBehaviour
             case EmoteType.Chew:
                 animator.ResetTrigger(_hashChew);
                 animator.SetTrigger(_hashChew);
+                break;
+
+            case EmoteType.Slay:
+                animator.ResetTrigger(_hashSlay);
+                animator.SetTrigger(_hashSlay);
+                break;
+
+            case EmoteType.Thumbsup:
+                animator.ResetTrigger(_hashThumbsup);
+                animator.SetTrigger(_hashThumbsup);
                 break;
         }
     }
